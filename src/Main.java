651f8c7b17acc72512d91ae20694443800138c94
Adam1551 <adam1.johannesson@gmail.com>
2022-09-16 10:14:54 +0200

    added print hello

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String sayHello = "Hej och godmorgon!";
         System.out.println(sayHello);
+        System.out.println("Hello");
 
         Animal horse = new Animal(4, "Åke");
     }
